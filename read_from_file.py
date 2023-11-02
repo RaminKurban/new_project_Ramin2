ccaab1ab42e6631817fc8ca77a5c7d2779917cf7
Alex <Alex@ya.ru>
2023-11-02 19:33:42 +0300

    добавил  в файл read_from_file.py, перенос файла

---
+++ read_from_file.py
@@ -0,0 +1,6 @@
+# f = open - перенос файла
+f = open('stroki,cikli.py', 'r')
+for line in f:
+    print(line)
+f.close()
+# f.close() - закрытие задачи
---
+++ stroki,cikli.py
@@ -0,0 +1,5 @@
+kirill:morozov:m:89115674388
+ivan:urgant:m:89654567832
+aleksey:ivanov:m:89675432378
+anna:sidorova:f:89765436788
+svetlana:petrova:f:89115679081
