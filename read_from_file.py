a6929ec7ccc347087d3e471b87dbb6733ce90b3c
Alex <Alex@ya.ru>
2023-11-02 20:42:54 +0300

    добавил  в файл read_from_file.py, новые возможности 2

--- read_from_file.py
+++ read_from_file.py
@@ -8,7 +8,9 @@ for line in f:
     surname = split_line[1]
     sex = split_line[2]
     phone = split_line[3]
-    print(f'Привет, меня зовут {name}, моя фамилия {surname},пол {sex},телефон {phone} ')
+    #print(f'Привет, меня зовут {name}, моя фамилия {surname},пол {sex},телефон {phone} ')
+    insert_string = f'insert into telsprav values("{name}","{surname}","{sex}","{phone}" )'
+    print(insert_string)
 
 f.close()
 # f.close() - закрытие задачи
