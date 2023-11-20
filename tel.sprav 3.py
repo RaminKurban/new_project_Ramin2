9d012219b6d458537335e262e0fabd69b5a2840e
User <kurbanaslan@mail.ru>
2023-11-20 21:39:33 +0300

    новые методы 2 . База данных и добавление словаря

--- tel.sprav 3.py
+++ tel.sprav 3.py
@@ -1,22 +1,34 @@
+# capitalize - выравнивание букв, большая или маленькая
 import pymysql
+import  datetime
 
-
+phone_cache = {}
 
 while True:
-    connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test', port=33306)
-    cursor = connection.cursor()
-    username = input('Ведите имя абонента:')
-    cursor.execute(f'SELECT name , phone from telsprav WHERE name = "{username}"')
-    connection.close()
-    data = cursor.fetchall()
-    print(data)
-    #print(data[0][0],data[0][1])
-    print(len(data))
-    if len(data)> 0:
+
+    username = input('Ведите имя абонента:').capitalize()
+    start_time = datetime.datetime.now()
+
+    phone = phone_cache.get(username, None)
+    if  phone != None:
+        print(f'Телефон абонента {username} - {[phone]}')
+        print('из словаря')
+
+    else:
+        connection = pymysql.connect(host='nadejnei.net', user='student', password='1q2w#E$R', database='test',
+                                     port=33306)
+        cursor = connection.cursor()
+        cursor.execute(f'SELECT name , phone from telsprav WHERE name = "{username}"')
+        connection.close()
+        data = cursor.fetchall()
+    if len(data) > 0:
         name = data[0][0]
         phone = data[0][1]
         print(f'Телефон абонента {username} - {[phone]}')
+        print('из БЗ')
+        phone_cache[username] = phone
     else:
         print(f'Телефон абонента {username} - не найден ')
-
-
+end_time = datetime.datetime.now()
+work_time = email - start_time
+print(work_time)
